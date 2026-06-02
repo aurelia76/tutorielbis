6336f22c90ace6c1997a2cd556b7987f51a3a9bc
aurelia76 <aurelia.rabia@wanadoo.fr>
2026-06-02 15:23:52 +0200

    Add OrdrePriorite method for priority operations

--- numbers.cs
+++ numbers.cs
@@ -14,8 +14,27 @@ class Numbers
     int f=a/b;
     Console.WriteLine(f);
   }
+  void OrdrePriorite()
+  {
+    int a=5;
+    int b=4;
+    int c = 2;
+    int d= a+b*c;
+    Console.WriteLine(d);
+    d=(a+b)*c;
+    Console.WriteLine(d);
+    d=(a+b)-6*c+(12*4)/3+12;
+    Console.WriteLine(d);
+    
+    int e=7;
+    int f=4;
+    int g=3;
+    int h = (e+f)/g;
+    Console.WriteLine(h);
+  }
   static void Main()
   {
    nombreEntiers();
+    OrdrePriorite();
   }
 }

839d8d04809007f1cf866409083d7f606466b6af
aurelia76 <aurelia.rabia@wanadoo.fr>
2026-06-19 15:34:11 +0200

    Refactor anonymous type to tuple in tuples.cs

--- tuples.cs
+++ tuples.cs
@@ -4,10 +4,10 @@ class tuples
 {
   static void Main()
   {
-    var pt = new {X:1, Y:2};
+    var pt = (X:1, Y:2);
     var slope = (double)pt.Y/(double) pt.X;
     Console.WriteLine($"A line form the origin to the point{pt} has a slope of {slope}");
-    pt = new {X: pt.X+5, Y:pt.Y};
+    //pt = new {X: pt.X+5, Y:pt.Y};
     Console.WriteLine($"point is now at {pt}");
     var pt2= pt with {Y=10};
     Console.WriteLine($"the point 'pt2' est a {pt2}");

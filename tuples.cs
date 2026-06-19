013b0aa3e0923e48d64f0090be5d478819be2be9
aurelia76 <aurelia.rabia@wanadoo.fr>
2026-06-19 15:10:10 +0200

    Fix tuple initialization syntax in tuples.cs

--- tuples.cs
+++ tuples.cs
@@ -4,7 +4,7 @@ class tuples
 {
   static void Main()
   {
-    var pt = {X:1, Y:2};
+    var pt = new {X:1, Y:2};
     var slope = (double)pt.Y/(double) pt.X;
     Console.WriteLine($"A line form the origin to the point{pt} has a slope of {slope}");
   }

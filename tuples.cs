8da8d77a139fbe46d6edbe79fa61966e735dc616
aurelia76 <aurelia.rabia@wanadoo.fr>
2026-06-19 15:22:15 +0200

    Update point coordinates and print new position

--- tuples.cs
+++ tuples.cs
@@ -7,5 +7,7 @@ class tuples
     var pt = new {X:1, Y:2};
     var slope = (double)pt.Y/(double) pt.X;
     Console.WriteLine($"A line form the origin to the point{pt} has a slope of {slope}");
+    pt.X = pt.X+5;
+    Console.WriteLine($"point is now at {pt}");
   }
 }

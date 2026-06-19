e537722ea403098e8efcacafba0576807dce2e47
aurelia76 <aurelia.rabia@wanadoo.fr>
2026-06-19 15:24:48 +0200

    Fix reassignment of point coordinates in tuples.cs

--- tuples.cs
+++ tuples.cs
@@ -7,7 +7,7 @@ class tuples
     var pt = new {X:1, Y:2};
     var slope = (double)pt.Y/(double) pt.X;
     Console.WriteLine($"A line form the origin to the point{pt} has a slope of {slope}");
-    pt.X = pt.X+5;
+    pt = new {X: pt.X+5, Y:pt.Y};
     Console.WriteLine($"point is now at {pt}");
   }
 }

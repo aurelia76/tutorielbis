4b884ad957f17d78da21e08bdda420430253f671
aurelia76 <aurelia.rabia@wanadoo.fr>
2026-06-19 15:29:06 +0200

    Add new point transformation and logging

--- tuples.cs
+++ tuples.cs
@@ -9,5 +9,7 @@ class tuples
     Console.WriteLine($"A line form the origin to the point{pt} has a slope of {slope}");
     pt = new {X: pt.X+5, Y:pt.Y};
     Console.WriteLine($"point is now at {pt}");
+    var pt2= pt with {Y=10};
+    Console.WriteLine($"the point'pt2'est a {pt2}");
   }
 }

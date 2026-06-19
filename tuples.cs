47a0e26bdac527f94b207fd312e1b52b6e6ca871
aurelia76 <aurelia.rabia@wanadoo.fr>
2026-06-19 15:37:21 +0200

    Fix tuple assignment and create new tuple with Y=10

--- tuples.cs
+++ tuples.cs
@@ -7,9 +7,9 @@ class tuples
     var pt = (X:1, Y:2);
     var slope = (double)pt.Y/(double) pt.X;
     Console.WriteLine($"A line form the origin to the point{pt} has a slope of {slope}");
-    //pt = new {X: pt.X+5, Y:pt.Y};
+    pt = pt.X+5;
     Console.WriteLine($"point is now at {pt}");
-    var pt2= pt with {Y=10};
+    var pt2= (pt.X,Y:10);
     Console.WriteLine($"the point 'pt2' est a {pt2}");
   }
 }

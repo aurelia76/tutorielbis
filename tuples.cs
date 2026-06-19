1a596c64893c9e767dacfc39539dc378c1511d1f
aurelia76 <aurelia.rabia@wanadoo.fr>
2026-06-19 15:07:34 +0200

    Fix method name and Console.WriteLine syntax

--- tuples.cs
+++ tuples.cs
@@ -2,10 +2,10 @@ using System;
 
 class tuples
 {
-  static void main()
+  static void Main()
   {
     var pt = {X:1, Y:2};
     var slope = (double)pt.Y/(double) pt.X;
-    Console.writeLine($"A line form the origin to the point{pt} has a slope of {slope}");
+    Console.WriteLine($"A line form the origin to the point{pt} has a slope of {slope}");
   }
 }

3af763450e23f22e902f9b7542b0dbc63861a6b6
aurelia76 <aurelia.rabia@wanadoo.fr>
2026-06-22 15:31:01 +0200

    Refactor Point record declaration in types.cs
    Moved the Point record declaration outside of the Main method.

--- types.cs
+++ types.cs
@@ -1,9 +1,9 @@
 using System;
+public record Point(int X, int Y);
 class types
 {
   static void Main()
   {
-    public record Point(int X, int Y);
     Point pt3 = new Point(1,1);
     //var pt4 = pt3 with {Y=10};
     Console.WriteLine($"les deux points sont {pt3} et ");

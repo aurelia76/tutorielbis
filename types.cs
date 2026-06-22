ad34386d9b48cc6d667e3dbbdbf87fb8eb15be42
aurelia76 <aurelia.rabia@wanadoo.fr>
2026-06-22 15:34:21 +0200

    Fix output format in Console.WriteLine

--- types.cs
+++ types.cs
@@ -6,6 +6,6 @@ class types
   {
     Point pt3 = new Point(1,1);
     var pt4 = pt3 with {Y=10};
-    Console.WriteLine($"les deux points sont {pt3} et {pt4}");
+    Console.WriteLine($"les deux points sont {pt3} et {pt4}.");
   }
 }

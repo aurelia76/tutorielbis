164cfcf18c233bfa8eab9c51d4cee3ca3df6a256
aurelia76 <aurelia.rabia@wanadoo.fr>
2026-06-22 15:28:24 +0200

    Comment out point modification and update output
    Comment out the creation of pt4 and adjust output.

--- types.cs
+++ types.cs
@@ -5,7 +5,7 @@ class types
   {
     public record Point(int X, int Y);
     Point pt3 = new Point(1,1);
-    var pt4 = pt3 with {Y=10};
-    Console.WriteLine($"les deux points sont {pt3} et {pt4}");
+    //var pt4 = pt3 with {Y=10};
+    Console.WriteLine($"les deux points sont {pt3} et ");
   }
 }

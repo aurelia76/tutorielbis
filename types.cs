a61e7cea7acfc25c5d62c77f4f9e7adeb43094eb
aurelia76 <aurelia.rabia@wanadoo.fr>
2026-06-22 15:33:15 +0200

    Enable point modification and display both points

--- types.cs
+++ types.cs
@@ -5,7 +5,7 @@ class types
   static void Main()
   {
     Point pt3 = new Point(1,1);
-    //var pt4 = pt3 with {Y=10};
-    Console.WriteLine($"les deux points sont {pt3} et ");
+    var pt4 = pt3 with {Y=10};
+    Console.WriteLine($"les deux points sont {pt3} et {pt4}");
   }
 }

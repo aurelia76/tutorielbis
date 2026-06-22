5abc7989c5591e5c2700a252dab7d75090369b57
aurelia76 <aurelia.rabia@wanadoo.fr>
2026-06-22 15:43:21 +0200

    Add Slope method to Point record

--- types.cs
+++ types.cs
@@ -1,5 +1,6 @@
 using System;
 public record Point(int X, int Y);
+public double Slope()=>(double)X/ (double)Y;
 class types
 {
   static void Main()
@@ -7,5 +8,7 @@ class types
     Point pt3 = new Point(1,1);
     var pt4 = pt3 with {Y=10};
     Console.WriteLine($"les deux points sont {pt3} et {pt4}.");
+    double slopeResult = pt4.Slope();
+    Console.WriteLine($"the slope of {pt4} is {slopeResult}"); 
   }
 }
